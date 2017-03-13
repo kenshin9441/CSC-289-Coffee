d8104e29a19de86b294cec290a04d5d34664b08a
praykor <praykor@localhost>
2017-03-12 21:11:33 -0400

    Create DB access class. Class to open DB

---
+++ CoffeShop/src/coffeshop/DBAccessor.java
@@ -0,0 +1,14 @@
+/*
+* This class is meant to be used to access the database
+* Simply use the methods inside it, and get your data back.
+* Can also be used to create reports
+ */
+package coffeshop;
+
+/**
+ *
+ * @author praykor
+ */
+public class DBAccessor {
+    
+}
