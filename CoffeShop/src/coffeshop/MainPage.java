744dfc716ddd74a3ac6e20f4b98d3155d4b0a938
praykor <gonzalezg1940@students.forsythtech.edu>
2017-03-13 18:58:56 -0400

    create main page

--- CoffeShop/src/coffeshop/MainPage.java
+++ CoffeShop/src/coffeshop/MainPage.java
@@ -1,6 +1,7 @@
 /*
- *
- *
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package coffeshop;
 
@@ -26,119 +27,21 @@ public class MainPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jlogo = new javax.swing.JLabel();
-        jpLogin = new javax.swing.JPanel();
-        jlUsernameLabel = new javax.swing.JLabel();
-        jlPasswordLabel = new javax.swing.JLabel();
-        jtFUsernameBox = new javax.swing.JTextField();
-        jpfPassowordBox = new javax.swing.JPasswordField();
-        jbLogin = new javax.swing.JButton();
-        jbForget = new javax.swing.JButton();
-        jlTime = new javax.swing.JLabel();
-        jLDate = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Login");
-        setResizable(false);
-
-        jlogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImageRes/logo.png"))); // NOI18N
-
-        jpLogin.setBorder(javax.swing.BorderFactory.createTitledBorder("Login"));
-
-        jlUsernameLabel.setText("Username:");
-
-        jlPasswordLabel.setText("Password:");
-
-        jbLogin.setText("Login");
-        jbLogin.setBorder(null);
-        jbLogin.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbLoginActionPerformed(evt);
-            }
-        });
-
-        jbForget.setText("Forgot username or password?");
-        jbForget.setBorder(null);
-        jbForget.setContentAreaFilled(false);
-
-        javax.swing.GroupLayout jpLoginLayout = new javax.swing.GroupLayout(jpLogin);
-        jpLogin.setLayout(jpLoginLayout);
-        jpLoginLayout.setHorizontalGroup(
-            jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jpLoginLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jlUsernameLabel)
-                    .addComponent(jlPasswordLabel)
-                    .addComponent(jbLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(58, 58, 58)
-                .addGroup(jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jbForget)
-                    .addGroup(jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(jpfPassowordBox, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jtFUsernameBox, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(36, Short.MAX_VALUE))
-        );
-        jpLoginLayout.setVerticalGroup(
-            jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpLoginLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jtFUsernameBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jlUsernameLabel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jlPasswordLabel)
-                    .addComponent(jpfPassowordBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(27, 27, 27)
-                .addGroup(jpLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jbLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbForget))
-                .addContainerGap(13, Short.MAX_VALUE))
-        );
-
-        jlTime.setText("Time: 00:00:00");
-
-        jLDate.setText("Date: dd/mm/yy");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(94, 94, 94)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jlTime)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLDate)
-                        .addGap(143, 143, 143))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jlogo)
-                            .addComponent(jpLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addContainerGap(117, Short.MAX_VALUE))))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addComponent(jlogo)
-                .addGap(18, 18, 18)
-                .addComponent(jpLogin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLDate)
-                    .addComponent(jlTime))
-                .addGap(18, 18, 18))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jbLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbLoginActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jbLoginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -176,15 +79,5 @@ public class MainPage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLDate;
-    private javax.swing.JButton jbForget;
-    private javax.swing.JButton jbLogin;
-    private javax.swing.JLabel jlPasswordLabel;
-    private javax.swing.JLabel jlTime;
-    private javax.swing.JLabel jlUsernameLabel;
-    private javax.swing.JLabel jlogo;
-    private javax.swing.JPanel jpLogin;
-    private javax.swing.JPasswordField jpfPassowordBox;
-    private javax.swing.JTextField jtFUsernameBox;
     // End of variables declaration//GEN-END:variables
 }
