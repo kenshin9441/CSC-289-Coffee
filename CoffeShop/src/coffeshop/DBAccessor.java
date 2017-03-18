8406852005fb3d12155150c75af48d1ab7dc3edf
praykor <gonzalezg1940@students.forsythtech.edu>
2017-03-17 20:19:27 -0400

    sql connection data

--- CoffeShop/src/coffeshop/DBAccessor.java
+++ CoffeShop/src/coffeshop/DBAccessor.java
@@ -5,10 +5,38 @@
  */
 package coffeshop;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author praykor
  */
 public class DBAccessor {
+    public static void main(String args[]){
+    final String DATABASE_URL = "jdbc:mysql://192.168.30.23/penguino";
+    final String SELECT_QUERY = "SELECT * FROM Penguino";
+    final String USERNAME = "root@192.168.30.20 ";
+    final String PASSWORD = "1Password!";
+    String results = "";
+    String sql = "SELECT * FROM Files";
+    PreparedStatement prep;
     
+    
+    try{
+        Connection connection = DriverManager.getConnection(DATABASE_URL);
+        prep = connection.prepareStatement("SELECT * FROM Penguino");
+        ResultSet data = prep.executeQuery();
+        
+        ResultSet result = prep.executeQuery();
+        System.out.println(result);
+    }catch(SQLException ex){
+        System.out.print(ex);
+    }
+    }
 }
